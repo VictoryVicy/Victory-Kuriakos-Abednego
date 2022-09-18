60c350fed0cb78ce19c7a63ed5c12717c3e52faa
VictoryVicy <113294309+VictoryVicy@users.noreply.github.com>
2022-09-18 20:41:26 +0700

    Create bank.py

---
+++ bank.py
@@ -0,0 +1,10 @@
+g=input(" ").lower().replace(" ","")
+x=g[0:5]
+if(x=="hello"):
+    print("$0")
+elif(g=="hello, newman"):
+    print("$0")
+elif(x[0]=="h"):
+    print("$20")
+else :
+    print("$100")
